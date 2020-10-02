b73714c52dea81adaa2eda111be93f3df373fc7b
soroosh-gb <soroosh.gb@gmail.com>
2020-10-02 00:21:17 -0400

    midnight fix

--- beats-api/app/controllers/api/v1/beats_controller.rb
+++ beats-api/app/controllers/api/v1/beats_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::BeatsController < ApplicationController
-    before_action :find_beat, only: [:show, :update, :destroy]
+
     def index 
         beats = Beat.all 
         render json: beats 
     end
 
     def show
-        render json: @beat, status: 200
+        beat  = Beat.find(params[:id])
+
+        render json: beat
     end
 
     def create
@@ -21,18 +23,13 @@ class Api::V1::BeatsController < ApplicationController
         render json: beat
     end
 
-    # def destroy
-
-    # end
+    
 
     private
 
     def beat_params
-        params.require(:beat).permit(:name, :tempo, :steps)
+        params.require(:beat).permit!
     end
 
-    def find_beat
-        beat= Beat.find(params[:id])
-    end
 
 end
--- beats-api/app/controllers/api/v1/users_controller.rb
+++ beats-api/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,23 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: users
     end  
+
+    def show
+        user  = User.find(params[:id])
+
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+
+        render json: user
+    end
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit!
+    end
 end
